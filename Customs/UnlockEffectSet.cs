04da78e38deea403fc65a46ded6d50c15e9e1b99
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-04-19 13:06:31 +0800

    Added Unlock related registry items

---
+++ Customs/UnlockEffectSet.cs
@@ -0,0 +1,20 @@
+﻿using KitchenData;
+using System.Collections.Generic;
+
+namespace CodelessModInterop.Customs
+{
+    public class UnlockEffectSet
+    {
+        public bool IsReadOnly { get; protected set; }
+
+        protected List<UnlockEffect> _unlockEffects = new List<UnlockEffect>();
+
+        public IEnumerable<UnlockEffect> UnlockEffects => IsReadOnly? new List<UnlockEffect>(_unlockEffects) : _unlockEffects;
+
+        public UnlockEffectSet(IEnumerable<UnlockEffect> unlockEffects, bool readOnly = true)
+        {
+            _unlockEffects = new List<UnlockEffect>(unlockEffects);
+            IsReadOnly = readOnly;
+        }
+    }
+}
--- ModdedResourceRegistry.cs
+++ ModdedResourceRegistry.cs
@@ -2,6 +2,7 @@
 using KitchenLib.Customs;
 using System.Collections.Generic;
 using UnityEngine;
+using CodelessModInterop.Customs;
 
 namespace CodelessModInterop
 {
@@ -9,6 +10,9 @@ namespace CodelessModInterop
     {
         private static Dictionary<int, string> _moddedGDO = new Dictionary<int, string>();  // Value is mod name the GDO belongs to
         private static Dictionary<string, Material> _moddedMaterials = new Dictionary<string, Material>();
+        private static Dictionary<string, UnlockEffectSet> _unlockEffectSets = new Dictionary<string, UnlockEffectSet>();
+        private static Dictionary<string, Localisation> _localisations = new Dictionary<string, Localisation>();
+        private static Dictionary<string, IEffectType> _effectTypes = new Dictionary<string, IEffectType>();
 
         public static bool TryGetModdedGDO<T>(int id, out T gdo, out string modName, bool codelessOnly = false) where T : GameDataObject
         {
@@ -53,6 +57,7 @@ namespace CodelessModInterop
             {
                 return true;
             }
+            Main.LogWarning($"Failed to find Material with Name {materialName}.");
             material = null;
             return false;
         }
@@ -66,10 +71,77 @@ namespace CodelessModInterop
         {
             if (_moddedMaterials.ContainsKey(material.name))
             {
-                Main.LogWarning($"Modded material with Name {material.name} already registered.");
+                Main.LogWarning($"Modded Material with Name {material.name} already registered.");
                 return false;
             }
             _moddedMaterials.Add(material.name, material);
+            return true;
+        }
+
+        public static bool TryGetModdedUnlockEffectSet(string unlockEffectSetName, out IEnumerable<UnlockEffect> unlockEffects)
+        {
+            if (_unlockEffectSets.TryGetValue(unlockEffectSetName, out UnlockEffectSet unlockEffectSet))
+            {
+                unlockEffects = unlockEffectSet.UnlockEffects;
+                return true;
+            }
+            Main.LogWarning($"Failed to find UnlockEffectSet with Name {unlockEffectSetName}.");
+            unlockEffects = null;
+            return false;
+        }
+
+        public static bool RegisterModdedUnlockEffectSet(string unlockEffectSetName, IEnumerable<UnlockEffect> unlockEffects)
+        {
+            if (_unlockEffectSets.ContainsKey(unlockEffectSetName))
+            {
+                Main.LogWarning($"Modded UnlockEffectSet with Name {unlockEffectSetName} already registered.");
+                return false;
+            }
+            _unlockEffectSets.Add(unlockEffectSetName, new UnlockEffectSet(unlockEffects));
+            return true;
+        }
+
+        public static bool TryGetModdedLocalisation(string localisationName, out Localisation localisation)
+        {
+            if (_localisations.TryGetValue(localisationName, out localisation))
+            {
+                return true;
+            }
+            Main.LogWarning($"Failed to find Localisation with Name {localisationName}.");
+            localisation = null;
+            return false;
+        }
+
+        public static bool RegisterModdedLocalisation(string localisationName, Localisation localisation)
+        {
+            if (_localisations.ContainsKey(localisationName))
+            {
+                Main.LogWarning($"Modded Localisation with Name {localisationName} already registered.");
+                return false;
+            }
+            _localisations.Add(localisationName, localisation);
+            return true;
+        }
+
+        public static bool TryGetModdedEffectType(string effectTypeName, out IEffectType effectType)
+        {
+            if (_effectTypes.TryGetValue(effectTypeName, out effectType))
+            {
+                return true;
+            }
+            Main.LogWarning($"Failed to find EffectType with Name {effectTypeName}.");
+            effectType = null;
+            return false;
+        }
+
+        public static bool RegisterModdedEffectType(string effectTypeName, IEffectType effectType)
+        {
+            if (_effectTypes.ContainsKey(effectTypeName))
+            {
+                Main.LogWarning($"Modded EffectType with Name {effectTypeName} already registered.");
+                return false;
+            }
+            _effectTypes.Add(effectTypeName, effectType);
             return true;
         }
     }
